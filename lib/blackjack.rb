fed4c55ea93a1facf44b7af477b3df12eebb33e6
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-10 14:05:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-cards = 2.times do deal_card
+cards = deal_card + deal_card
 
   # total = display_card_total(deal_card + deal_card)
   # # return card_total
