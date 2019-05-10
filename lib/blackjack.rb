888571b6e160cb81b6b5ccccb665418fd22b29a9
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-10 15:20:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,12 +50,12 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  welcome
-  initial_round
-  current_total = hit?
-  until current_total > 21 do
-    hit?
-  end
-  end_game
-end
+# def runner
+#   welcome
+#   initial_round
+#   current_total = hit?
+#   until current_total > 21 do
+#     hit?
+#   end
+#   end_game
+# end
