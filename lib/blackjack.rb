5f5a72b378c843143069e7c10fb09d5772b0f102
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-10 14:13:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,7 +7,7 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
+  print "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -24,7 +24,7 @@ end
 
 def initial_round
   return cards = deal_card + deal_card
-  print display_card_total(cards)
+  display_card_total(cards)
 
 
   # total = display_card_total(deal_card + deal_card)
